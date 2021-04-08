173695c1dbc2a46efd0cfe0c1a9812401b47642e
04D4BC2AE3759789CB2D61708E8CBCEE <04D4BC2AE3759789CB2D61708E8CBCEE@i-search.com.cn>
2021-04-08 14:50:46 +0800

    add

--- src/main/java/com/szcinda/rpa/netty/NettyChannelInitializer.java
+++ src/main/java/com/szcinda/rpa/netty/NettyChannelInitializer.java
@@ -14,18 +14,12 @@ public class NettyChannelInitializer extends ChannelInitializer<SocketChannel> {
 
     @Override
     protected void initChannel(SocketChannel ch) {
-        // ChannelOutboundHandler，依照逆序执行
-        ch.pipeline().addLast("encoder", new StringEncoder());
-
-        // 属于ChannelInboundHandler，依照顺序执行
-        ch.pipeline().addLast("decoder", new StringDecoder());
         ChannelPipeline pipeline = ch.pipeline();
         // 处理粘包问题
         ByteBuf delimiter = Unpooled.copiedBuffer("$$".getBytes());
         pipeline.addLast("framer", new DelimiterBasedFrameDecoder(9056, delimiter));
-        /**
-         * 自定义ChannelInboundHandlerAdapter
-         */
+        ch.pipeline().addLast("encoder", new StringEncoder());
+        ch.pipeline().addLast("decoder", new StringDecoder());
         ch.pipeline().addLast(new NettyChannelInboundHandlerAdapter());
     }
 }
